fa390b4444daf75787deedcc154e5ee8f7600778
BIRADARGB <basanagoudabiradar2@gmail.com>
2023-08-28 17:24:03 +0530

    third commit

---
+++ src/main/java/Tests/MyTestClass.java
@@ -0,0 +1,31 @@
+package Tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+public class MyTestClass {
+    ChromeDriver driver;
+
+    //sample project
+    @BeforeTest
+    public void setUp(){
+        System.out.println("Test case starting now...");
+    }
+    @Test
+    public void print() throws InterruptedException {
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        System.out.println("welcome to selenium");
+        driver.get("https://www.flipkart.com");
+        Thread.sleep(3000);
+    }
+
+    @AfterTest
+    public void tearDown(){
+        driver.quit();
+    }
+}
+
