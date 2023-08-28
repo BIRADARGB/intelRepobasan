2d74ee56fc4817a2e0a790811f07701b3ee6502d
BIRADARGB <basanagoudabiradar2@gmail.com>
2023-08-28 16:17:00 +0530

    first commit

---
+++ src/Packjava/MyNewClass.java
@@ -0,0 +1,27 @@
+package Packjava;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class MyNewClass {
+    WebDriver driver;
+
+@Test
+    public void print(){
+        System.out.println("Hi");
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://www.flipkart.com");
+    }
+
+    @AfterTest
+    public void tearDown() throws InterruptedException {
+    Thread.sleep(3000);
+    driver.quit();
+    }
+}
